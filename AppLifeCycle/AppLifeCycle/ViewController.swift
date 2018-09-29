cf953e1d755af3f70413fbc4d4ee6cfe0c2cbf2f
Yookyung Kim <tituvely@gmail.com>
2018-09-29 22:04:26 +0900

    Upload AppLifeCycle

---
+++ AppLifeCycle/AppLifeCycle/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  AppLifeCycle
+//
+//  Created by YooKyung Kim on 29/09/2018.
+//  Copyright © 2018 YooKyung Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("viewDidLoad")
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        print("viewWillAppear")
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        print("viewDidAppear")
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        print("viewWillDisappear")
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        print("viewDidDisappear")
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
