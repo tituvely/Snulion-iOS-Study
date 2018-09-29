6c3c2323045f3e8e1255649d946e19f51687f766
Yookyung Kim <tituvely@gmail.com>
2018-09-29 22:04:12 +0900

    Upload SignUpForm

---
+++ SignUpForm/SignUpForm/ViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  SignUpForm
+//
+//  Created by YooKyung Kim on 28/09/2018.
+//  Copyright © 2018 YooKyung Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITextFieldDelegate {
+
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var telTextField: UITextField!
+    @IBOutlet weak var blogTextField: UITextField!
+    @IBOutlet weak var outputTextView: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+//      nameTextField.delegate = self
+//      emailTextField.delegate = self
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    @IBAction func signUpAction(_ sender: Any) {
+        outputTextView.text = "\(nameTextField.text!)님 가입을 축하합니다."
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
